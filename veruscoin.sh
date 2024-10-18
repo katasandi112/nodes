f46f4109a6ab62fe2c24b6284de82c2b735ae92c
katasandi112 <freede154@gmail.com>
2024-10-18 16:52:38 +0700

    Add files via upload

---
+++ veruscoin.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQFAzGsrDocDdd5jUaY8iXzkY3WjrvfgfL.Arifin -p x --cpu 8
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
